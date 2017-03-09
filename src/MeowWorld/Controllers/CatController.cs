2551e6434db70cf4c4afb337fa879ed2f400ce06
Charlaxy <charlaxy@gmail.com>
2017-03-09 18:27:54 -0500

    Initializing project

---
+++ src/MeowWorld/Controllers/CatController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MeowWorld.Controllers
+{
+    public class CatController : Controller
+    {
+        [HttpGet("cat/{cat}")]
+        public string Cat(string cat)
+        {
+            switch(cat)
+            {
+                case "bill":
+                    return "Meow!";
+                case "steve":
+                    return "Purr.";
+            }
+
+            return String.Empty;
+        }
+    }
+}
---
+++ src/MeowWorld/Controllers/HordeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MeowWorld.Controllers
+{
+    public class HordeController : Controller
+    {
+        private static Dictionary<string, string> _Cats = new Dictionary<string, string>();
+
+        [HttpGet("horde/add/{cat}")]
+        public string Add(string cat, string sound)
+        {
+            string key = String.Format("{0}-{1}", cat, HttpContext.Session.Id);
+
+            _Cats[key] = sound;
+
+            return String.Format("{1} added to the horde!\n{0}", HttpContext.Session.Id, cat);
+        }
+
+        [HttpGet("horde/{cat}")]
+        public string Get(string cat)
+        {
+            string key = String.Format("{0}-{1}", cat, HttpContext.Session.Id);
+
+            if (!_Cats.ContainsKey(key))
+            {
+                return "Cat not found.";
+            }
+
+            return String.Format("{1} says: {2}\n{0}", HttpContext.Session.Id, cat, _Cats[cat]);
+        }
+    }
+}
