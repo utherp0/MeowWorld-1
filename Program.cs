d8089c84bcdfbf9680a95f66168dbecb77bf1377
Charlaxy <charlaxy@gmail.com>
2017-03-20 13:02:34 -0500

    Commented out IISIntegration

--- Program.cs
+++ Program.cs
@@ -15,7 +15,7 @@ namespace MeowWorld
             var host = new WebHostBuilder()
                 .UseKestrel()
                 .UseContentRoot(Directory.GetCurrentDirectory())
-                .UseIISIntegration()
+                //.UseIISIntegration()
                 .UseStartup<Startup>()
                 .Build();
 
