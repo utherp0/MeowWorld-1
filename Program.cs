0eee4a8247565228a0573ccd67087c7cb0b68542
Charlaxy <charlaxy@gmail.com>
2017-03-20 14:13:34 -0500

    Changed port settings, removed IISIntegration

--- Program.cs
+++ Program.cs
@@ -1,10 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Hosting;
+﻿using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using System.IO;
 
 namespace MeowWorld
 {
@@ -12,14 +9,19 @@ namespace MeowWorld
     {
         public static void Main(string[] args)
         {
+            var config = new ConfigurationBuilder().AddEnvironmentVariables("").Build();
+
+            var url = config["ASPNETCORE_URLS"] ?? "http://*:8080";
+
             var host = new WebHostBuilder()
                 .UseKestrel()
                 .UseContentRoot(Directory.GetCurrentDirectory())
                 //.UseIISIntegration()
+                .UseUrls(url)
                 .UseStartup<Startup>()
                 .Build();
 
-            host.Run();
+            host.Run(); 
         }
     }
 }
