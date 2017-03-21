0fe17d5c0aa103564b1ea0d361315392369dfa08
Charlaxy <charlaxy@gmail.com>
2017-03-21 11:06:26 -0500

    Added the UseUrls setting to the WebHostBuilder so it listens to port 8080.

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
 using System.IO;
 
 namespace MeowWorld
@@ -8,11 +9,16 @@ namespace MeowWorld
     {
         public static void Main(string[] args)
         {
+            var config = new ConfigurationBuilder().AddEnvironmentVariables("").Build();
+
+            var url = config["ASPNETCORE_URLS"] ?? "http://*:8080";
+
             var host = new WebHostBuilder()
                 .UseKestrel()
                 .UseContentRoot(Directory.GetCurrentDirectory())
                 .UseIISIntegration()
                 .UseStartup<Startup>()
+                .UseUrls(url)
                 .Build();
 
             host.Run();
