5d894977fe892e0ae07a5fb0b917efcf41ee1544
Charlaxy <charlaxy@gmail.com>
2017-03-23 16:47:19 -0500

    Added pre-compiler variables to make debugging at home easier.

--- Program.cs
+++ Program.cs
@@ -9,16 +9,19 @@ namespace MeowWorld
     {
         public static void Main(string[] args)
         {
+
+#if !DEBUG
             var config = new ConfigurationBuilder().AddEnvironmentVariables("").Build();
-
             var url = config["ASPNETCORE_URLS"] ?? "http://*:8080";
-
+#endif
             var host = new WebHostBuilder()
                 .UseKestrel()
                 .UseContentRoot(Directory.GetCurrentDirectory())
                 .UseIISIntegration()
                 .UseStartup<Startup>()
+#if !DEBUG
                 .UseUrls(url)
+#endif
                 .Build();
 
             host.Run();
